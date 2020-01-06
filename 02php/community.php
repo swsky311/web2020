8a3b7993ef0ef235155aa75e8a6d9062a554330e
swsky311 <58929557+swsky311@users.noreply.github.com>
2020-01-06 17:45:10 +0900

    today up

---
+++ 02php/community.php
@@ -0,0 +1,7 @@
+<?include "header.php"; ?>
+	<div class="sub community"></div>
+	<section id="container">
+		<div class="content"></div>
+		<aside class="menu"></aside>
+	</section>
+<? include "footer.php"; ?>
